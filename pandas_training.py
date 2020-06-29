01c52832353ee0487713882dd28609d634ddcf56
Umut Kapucu <31987929+umutkapucu@users.noreply.github.com>
2020-06-29 19:23:28 +0300

    training done

--- pandas_training.py
+++ pandas_training.py
@@ -10,4 +10,4 @@ import pandas as pd
 csv_path = 'https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/PY0101EN/Chapter%204/Datasets/TopSellingAlbums.csv'
 df = pd.read_csv(csv_path)
 
-df.loc[0.0]
+df.head()
